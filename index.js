840077920b098ec517701d3852ed31c52257a170
Claire Muller <clairermuller@gmail.com>
2018-12-25 09:10:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+var theBeatlesPlay = (musician, instrument) => {
+  var array = [];
+  for (let i = 0; i < musician.length; i++) {
+  array.push(`${musician[i]} plays ${instrument[i]}`);
+  }
+  return array;
+};
+
+
+var johnLennonFacts = (facts) => {
+  var i = 0;
+  var array = [];
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return array;
+};
+
+var iLoveTheBeatles = (num) = {
+  var array = [];
+};
+
+
+
